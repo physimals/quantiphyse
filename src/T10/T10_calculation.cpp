e98755eb87fd30fd5029bbc60ff7ba1054363ddf
Benjamin Irving <benjamin.irving@eng.ox.ac.uk>
2016-02-11 13:53:45 +0000

    add T10 files

---
+++ src/T10/T10_calculation.cpp
@@ -0,0 +1,87 @@
+//
+// Created by engs1170 on 07/01/16.
+//
+
+#include "T10_calculation.h"
+#include "linear_regression.h"
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+// TODO write a nonlinear version
+// TODO Smoothing
+// TODO Python/Cython wrapper
+
+// Perform VFA T1 mapping on a single voxel
+// Linear mapping may underestimate the T1 values
+double T10_single_linear(vector<double> &favox, vector<double> &fa_rad, ulong num_fa, double TR){
+
+    vector<double> x(num_fa, 0);
+    vector<double> y(num_fa, 0);
+    double a, b, v1, v2;
+    double t1;
+
+    for (int ii=0; ii<num_fa; ii++){
+        x[ii] = favox[ii] / tan(fa_rad[ii]);
+        y[ii] = favox[ii] / sin(fa_rad[ii]);
+    }
+
+    // Return intercept and gradient from linear regression.
+    tie(a, b) = linreg(y, x);
+
+    // requiring gradient to be greater than 0
+    if (b > 0) {
+        v1 = log(b);
+        t1 = -TR/v1;
+    }
+    else {
+        t1 = 0;
+    }
+
+    // TODO for testing purposes
+    if (t1 > 5.0){
+        t1 = 5.0;
+    }
+
+    if (t1 < 0) {
+        t1 = 0;
+    }
+    // Optional: calculate M0 as well
+    return t1;
+}
+
+// Run through an entire array to perform T10 mapping
+vector<double> T10mapping( vector< std::vector<double> > & favols, vector<double> & fa, double TR) {
+
+    ulong num_fa = fa.size();
+    ulong num_voxels = favols[0].size();
+
+    vector<double> fa_rad (num_fa, 0);
+    vector<double> favox(num_fa, 0);
+    vector<double> t10vec(num_voxels, 0);
+
+    // Convert flip angles to radians
+    cout << "Converting flip angles to radians \n";
+    int cc = 0;
+    for (double ii : fa) {
+        fa_rad[cc] = (ii * (M_PI/180));
+        cc++;
+    }
+
+    cout << "t10 calculation for " << num_voxels << " voxels \n";
+    // Loop through all voxels
+    for (int jj=0; jj < num_voxels; jj++){
+
+        // Store value at each fa
+        for (int kk=0; kk < num_fa; kk++){
+            favox[kk] = favols.at(kk).at(jj);
+        }
+
+        // Calculating T10
+        t10vec[jj] = T10_single_linear(favox, fa_rad, num_fa, TR);
+    }
+
+    return t10vec;
+}
---
+++ src/T10/T10_calculation.h
@@ -0,0 +1,15 @@
+//
+// Created by engs1170 on 07/01/16.
+//
+
+#ifndef INC_25_T10_CALCULATION_T10_CALCULATION_H
+#define INC_25_T10_CALCULATION_T10_CALCULATION_H
+
+#include <vector>
+//
+//
+// fa - flip angles (radians)
+std::vector<double> T10mapping( std::vector< std::vector<double> > & favols, std::vector<double> & fa, double TR = 1.0);
+
+
+#endif //INC_25_T10_CALCULATION_T10_CALCULATION_H
