3b924c25b7abe9866e491887d2db432ae2b5cacf
Benjamin Irving <benjamin.irving@eng.ox.ac.uk>
2016-03-16 11:59:02 +0000

    adding batch conversion

--- pkview/utils/cmd_pkmodel.py
+++ pkview/utils/cmd_pkmodel.py
@@ -29,122 +29,126 @@ def pool_init(queue):
 def pkbatch(yaml_file):
 
     # Load config from yaml
-    c1 = yaml_loader(yaml_file)
-    c1 = c1['RIT005']
+    c1_main = yaml_loader(yaml_file)
 
-    queue = multiprocessing.Queue()
-    pool = multiprocessing.Pool(processes=2, initializer=pool_init, initargs=(queue,))
+    for ii in c1_main.keys():
 
-    # get volumes to process
+        print(ii)
+        c1 = c1_main[ii]
 
-    print("Loading images")
-    img = nib.load(c1['Files']['DCE'])
-    img1 = img.get_data()
-    hdr = img.get_header()
-    img_dims = img1.shape
-    img = nib.load(c1['Files']['ROI'])
-    roi1 = img.get_data()
-    img = nib.load(c1['Files']['T10'])
-    t101 = img.get_data()
+        queue = multiprocessing.Queue()
+        pool = multiprocessing.Pool(processes=2, initializer=pool_init, initargs=(queue,))
 
-    print("Gettng parameters")
-    # Extract the text from the line edit options
-    R1 = c1['Param']['R1']
-    R2 = c1['Param']['R2']
-    DelT = c1['Param']['T']
-    InjT = c1['Param']['InjT']
-    TR = c1['Param']['TR']
-    TE = c1['Param']['TE']
-    FA = c1['Param']['flip_angle']
-    thresh1val = c1['Param']['v1thresh']
-    Dose = 0
+        # get volumes to process
 
-    # getting model choice from list
-    model_choice = c1['Configuration']['model_choice']
+        print("Loading images")
+        img = nib.load(c1['Files']['DCE'])
+        img1 = img.get_data()
+        hdr = img.get_header()
+        img_dims = img1.shape
+        img = nib.load(c1['Files']['ROI'])
+        roi1 = img.get_data()
+        img = nib.load(c1['Files']['T10'])
+        t101 = img.get_data()
 
-    baseline1 = np.mean(img1[:, :, :, :3], axis=-1)
+        print("Gettng parameters")
+        # Extract the text from the line edit options
+        R1 = c1['Param']['R1']
+        R2 = c1['Param']['R2']
+        DelT = c1['Param']['T']
+        InjT = c1['Param']['InjT']
+        TR = c1['Param']['TR']
+        TE = c1['Param']['TE']
+        FA = c1['Param']['flip_angle']
+        thresh1val = c1['Param']['v1thresh']
+        Dose = 0
 
-    print("Convert to list of enhancing voxels")
-    img1vec = np.reshape(img1, (-1, img1.shape[-1]))
-    T10vec = np.reshape(t101, (-1))
-    roi1vec = np.array(np.reshape(roi1, (-1)), dtype=bool)
-    baseline1 = np.reshape(baseline1, (-1))
+        # getting model choice from list
+        model_choice = c1['Configuration']['model_choice']
 
-    print("Make sure the type is correct")
-    img1vec = np.array(img1vec, dtype=np.double)
-    T101vec = np.array(T10vec, dtype=np.double)
-    roi1vec = np.array(roi1vec, dtype=bool)
+        baseline1 = np.mean(img1[:, :, :, :3], axis=-1)
 
-    print("subset")
-    # Subset within the ROI and
-    img1sub = img1vec[roi1vec, :]
-    T101sub = T101vec[roi1vec]
-    baseline1sub = baseline1[roi1vec]
+        print("Convert to list of enhancing voxels")
+        img1vec = np.reshape(img1, (-1, img1.shape[-1]))
+        T10vec = np.reshape(t101, (-1))
+        roi1vec = np.array(np.reshape(roi1, (-1)), dtype=bool)
+        baseline1 = np.reshape(baseline1, (-1))
 
-    # Normalisation of the image
-    img1sub = img1sub / (np.tile(np.expand_dims(baseline1sub, axis=-1), (1, img1.shape[-1])) + 0.001) - 1
+        print("Make sure the type is correct")
+        img1vec = np.array(img1vec, dtype=np.double)
+        T101vec = np.array(T10vec, dtype=np.double)
+        roi1vec = np.array(roi1vec, dtype=bool)
 
-    print("Running pk")
-    # run_pk(img1sub, T101sub, R1, R2, DelT, InjT, TR, TE, FA, Dose, model_choice)
-    #
-    result = pool.apply_async(func=run_pk, args=(img1sub, T101sub, R1, R2, DelT, InjT, TR, TE, FA, Dose, model_choice))
+        print("subset")
+        # Subset within the ROI and
+        img1sub = img1vec[roi1vec, :]
+        T101sub = T101vec[roi1vec]
+        baseline1sub = baseline1[roi1vec]
 
-    progress = 0
-    while progress < 100:
-        num_row, progress = queue.get()
-        print("Progress: ", progress)
-        time.sleep(5)
+        # Normalisation of the image
+        img1sub = img1sub / (np.tile(np.expand_dims(baseline1sub, axis=-1), (1, img1.shape[-1])) + 0.001) - 1
 
-    var1 = result.get()
+        print("Running pk")
+        # run_pk(img1sub, T101sub, R1, R2, DelT, InjT, TR, TE, FA, Dose, model_choice)
+        #
+        result = pool.apply_async(func=run_pk, args=(img1sub, T101sub, R1, R2, DelT, InjT, TR, TE, FA, Dose, model_choice))
 
-    roi1v = np.array(roi1vec, dtype=bool)
+        progress = 0
+        while progress < 100:
+            num_row, progress = queue.get()
+            print("Progress: ", progress)
+            time.sleep(5)
 
-    #Params: Ktrans, ve, offset, vp
-    Ktrans1 = np.zeros((roi1v.shape[0]))
+        var1 = result.get()
 
-    ktemp = var1[2][:, 0]
-    ktemp[ktemp < 0] = 0.0
-    ktemp[ktemp > 2] = 2.0
-    Ktrans1[roi1v] = ktemp
+        roi1v = np.array(roi1vec, dtype=bool)
 
-    ve1 = np.zeros((roi1v.shape[0]))
-    ve1[roi1v] = var1[2][:, 1] * (var1[2][:, 1] < 2.0) + 2 * (var1[2][:, 1] > 2.0)
-    ve1 *= (ve1 > 0)
+        #Params: Ktrans, ve, offset, vp
+        Ktrans1 = np.zeros((roi1v.shape[0]))
 
-    kep1p = Ktrans1 / (ve1 + 0.001)
-    kep1p[np.logical_or(np.isnan(kep1p), np.isinf(kep1p))] = 0
-    kep1p *= (kep1p > 0)
-    kep1 = kep1p * (kep1p < 2.0) + 2 * (kep1p >= 2.0)
+        ktemp = var1[2][:, 0]
+        ktemp[ktemp < 0] = 0.0
+        ktemp[ktemp > 2] = 2.0
+        Ktrans1[roi1v] = ktemp
 
-    offset1 = np.zeros((roi1v.shape[0]))
-    offset1[roi1v] = var1[2][:, 2]
+        ve1 = np.zeros((roi1v.shape[0]))
+        ve1[roi1v] = var1[2][:, 1] * (var1[2][:, 1] < 2.0) + 2 * (var1[2][:, 1] > 2.0)
+        ve1 *= (ve1 > 0)
 
-    vp1 = np.zeros((roi1v.shape[0]))
-    vp1[roi1v] = var1[2][:, 3]
+        kep1p = Ktrans1 / (ve1 + 0.001)
+        kep1p[np.logical_or(np.isnan(kep1p), np.isinf(kep1p))] = 0
+        kep1p *= (kep1p > 0)
+        kep1 = kep1p * (kep1p < 2.0) + 2 * (kep1p >= 2.0)
 
-    estimated_curve1 = np.zeros((roi1v.shape[0], img_dims[-1]))
-    estimated_curve1[roi1v, :] = var1[1]
+        offset1 = np.zeros((roi1v.shape[0]))
+        offset1[roi1v] = var1[2][:, 2]
 
-    residual1 = np.zeros((roi1v.shape[0]))
-    residual1[roi1v] = var1[0]
+        vp1 = np.zeros((roi1v.shape[0]))
+        vp1[roi1v] = var1[2][:, 3]
 
-    # Convert to list of enhancing voxels
-    Ktrans1vol = np.reshape(Ktrans1, (img_dims[:-1]))
-    ve1vol = np.reshape(ve1, (img_dims[:-1]))
-    offset1vol = np.reshape(offset1, (img_dims[:-1]))
-    vp1vol = np.reshape(vp1, (img_dims[:-1]))
-    kep1vol = np.reshape(kep1, (img_dims[:-1]))
-    estimated1vol = np.reshape(estimated_curve1, img_dims)
+        estimated_curve1 = np.zeros((roi1v.shape[0], img_dims[-1]))
+        estimated_curve1[roi1v, :] = var1[1]
 
-    # thresholding according to upper limit
-    # p = np.percentile(Ktrans1vol, thresh1val)
-    # Ktrans1vol[Ktrans1vol > p] = p
-    # p = np.percentile(kep1vol, thresh1val)
-    # kep1vol[kep1vol > p] = p
+        residual1 = np.zeros((roi1v.shape[0]))
+        residual1[roi1v] = var1[0]
 
-    print("Saving File")
-    save_file('Ktrans.nii', hdr, Ktrans1vol)
-    save_file('model_curves.nii', hdr, estimated1vol)
+        # Convert to list of enhancing voxels
+        Ktrans1vol = np.reshape(Ktrans1, (img_dims[:-1]))
+        ve1vol = np.reshape(ve1, (img_dims[:-1]))
+        offset1vol = np.reshape(offset1, (img_dims[:-1]))
+        vp1vol = np.reshape(vp1, (img_dims[:-1]))
+        kep1vol = np.reshape(kep1, (img_dims[:-1]))
+        estimated1vol = np.reshape(estimated_curve1, img_dims)
+
+        # thresholding according to upper limit
+        # p = np.percentile(Ktrans1vol, thresh1val)
+        # Ktrans1vol[Ktrans1vol > p] = p
+        # p = np.percentile(kep1vol, thresh1val)
+        # kep1vol[kep1vol > p] = p
+
+        print("Saving File")
+        save_file('Ktrans.nii', hdr, Ktrans1vol)
+        save_file('model_curves.nii', hdr, estimated1vol)
 
 
 
--- pkview/utils/cmd_t10.py
+++ pkview/utils/cmd_t10.py
@@ -17,61 +17,65 @@ from pkview.analysis.t1_model import t10_map
 def t10(yaml_file):
 
     # Load config from yaml
-    c1 = yaml_loader(yaml_file)
+    c1_main = yaml_loader(yaml_file)
 
-    # Just running on a single patient for the meanwhile
-    c1 = c1['EG1']
+    for ii in c1_main.keys():
 
-    fa_vols = []
-    fa_angles = []
+        print(ii)
+        # Just running on a single patient for the meanwhile
+        c1 = c1_main[ii]
 
-    for fa1 in c1['Files']:
+        fa_vols = []
+        fa_angles = []
 
-        img = nib.load(c1['Folder'] + fa1['File'])
-        hdr = img.get_header()
-        fa_vols.append(img.get_data())
-        fa_angles.append(fa1['FA'])
+        for fa1 in c1['Files']:
 
-    T10 = t10_map(fa_vols, fa_angles, TR=c1['Settings']['TR'])
+            img = nib.load(c1['Folder'] + fa1['File'])
+            hdr = img.get_header()
+            fa_vols.append(img.get_data())
+            fa_angles.append(fa1['FA'])
 
-    save_file(c1['Settings']['Out_file_path'], hdr, T10)
+        T10 = t10_map(fa_vols, fa_angles, TR=c1['Settings']['TR'])
+
+        save_file(c1['Settings']['Out_file_path'], hdr, T10)
 
 
 def t10_preclinical(yaml_file):
 
     # Load config from yaml
-    c1 = yaml_loader(yaml_file)
+    c1_main = yaml_loader(yaml_file)
 
-    # TODO Just running on a single patient for the meanwhile
-    c1 = c1['EG1']
+    for ii in c1_main.keys():
+        print(ii)
+        c1 = c1_main[ii]
 
-    fa_vols = []
-    afi_vols = []
+        fa_vols = []
+        afi_vols = []
 
-    img = nib.load(c1['Folder'] + c1['Files']['fa_vol'])
-    hdr = img.get_header()
-    img1 = img.get_data()
+        img = nib.load(c1['Folder'] + c1['Files']['fa_vol'])
+        hdr = img.get_header()
+        img1 = img.get_data()
 
-    print("Loading files")
-    for ii in range(len(c1['Settings']['FA'])):
-        # Need to convert it to a list of volumes to be consistent with clinical data
-        fa_vols.append(img1[:, :, :, ii])
-    fa_angles = c1['Settings']['FA']
+        print("Loading files")
+        for ii in range(len(c1['Settings']['FA'])):
+            # Need to convert it to a list of volumes to be consistent with clinical data
+            fa_vols.append(img1[:, :, :, ii])
+        fa_angles = c1['Settings']['FA']
 
-    img = nib.load(c1['Folder'] + c1['Files']['afi_vol'])
-    img1 = img.get_data()
+        img = nib.load(c1['Folder'] + c1['Files']['afi_vol'])
+        img1 = img.get_data()
 
-    for ii in range(len(c1['Settings']['TR_afi'])):
-        # Need to convert it to a list of volumes to be consistent with clinical data
-        afi_vols.append(img1[:, :, :, ii])
-    afi_angles = c1['Settings']['FA_afi']
+        for ii in range(len(c1['Settings']['TR_afi'])):
+            # Need to convert it to a list of volumes to be consistent with clinical data
+            afi_vols.append(img1[:, :, :, ii])
+        afi_angles = c1['Settings']['FA_afi']
 
-    print("Beginning conversion")
-    T10 = t10_map(fa_vols, fa_angles, TR=c1['Settings']['TR'],
-                  afi_vols=afi_vols, fa_afi=afi_angles, TR_afi=c1['Settings']['TR_afi'])
+        print("Beginning conversion")
+        T10 = t10_map(fa_vols, fa_angles, TR=c1['Settings']['TR'],
+                      afi_vols=afi_vols, fa_afi=afi_angles, TR_afi=c1['Settings']['TR_afi'])
 
-    print("Saving T10")
-    save_file(c1['Settings']['Out_file_path'], hdr, T10)
+        print("Saving T10")
+        save_file(c1['Settings']['Out_file_path'], hdr, T10)
 
 
 
