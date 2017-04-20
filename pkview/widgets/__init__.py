22412d19b99289d12ced246eaef9c4e63c0cee78
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-04-20 15:09:49 +0100

    Add hook functions for activation/deactivation

--- pkview/widgets/__init__.py
+++ pkview/widgets/__init__.py
@@ -13,12 +13,13 @@ class PkWidget(QtGui.QWidget):
     """
     Base class for a PkView widget
 
-    The following properties are set automatically:
+    The following properties are set automatically from keyword args or defaults:
       self.ivm - Image Volume Management instance
       self.ivl - ImageView instance
-      self.icon
-      self.name
-      self.description
+      self.icon - QIcon for the menu/tab
+      self.name - Name for the menu
+      self.desc - Longer description (for tooltip)
+      self.tabname - Name for the tab
     """
     def __init__(self, **kwargs):
         super(PkWidget, self).__init__()
@@ -29,14 +30,35 @@ class PkWidget(QtGui.QWidget):
         self.ivm = kwargs.get("ivm", None)
         self.ivl = kwargs.get("ivl", None)
         self.default = kwargs.get("default", False)
-	self.visible = False
+        self.visible = False
         self.tabname = kwargs.get("tabname", self.name.replace(" ", "\n"))
         if self.opts:
                 self.opts.sig_options_changed.connect(self.options_changed)
 
+    def init(self):
+        """
+        Called when widget is first shown. Widgets may choose to override this to delay
+        expensive setup operations until required
+        """
+        pass
+
+    def activate(self):
+        """
+        Called when widget is made active, so can for example connect signals to the 
+        volume management or view classes, and update it's current state
+        """
+        pass
+
+    def deactivate(self):
+        """
+        Called when widget is made inactive, so should for example disconnect signals and remove 
+        any related selections from the view
+        """
+        pass
+
     def options_changed(self):
         """
-        Override to respond to option changes
+        Override to respond to global option changes
         """
         pass
 
