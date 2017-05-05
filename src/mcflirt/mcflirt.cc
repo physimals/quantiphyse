36f7e375672f6df2b445350a301f35db4f5df365
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-05-05 09:41:57 +0100

    Fix memory leak

--- src/mcflirt/mcflirt.cc
+++ src/mcflirt/mcflirt.cc
@@ -859,5 +859,6 @@ std::string mcflirt_run (float *vol, std::vector<int> &extent, std::vector<float
   if (!globalopts. no_reporting) log << endl << "Saving motion corrected time series... " << endl;
   timeseries.setDisplayMaximumMinimum(timeseries.max(),timeseries.min());
 
+   if (Globaloptions::getInstance().impair != 0) delete Globaloptions::getInstance().impair;
   return log.str();
 }
