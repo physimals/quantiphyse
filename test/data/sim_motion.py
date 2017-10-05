f321aa1f3dbb8ae3e74622c5063f041129b88f41
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-10-05 14:06:29 +0100

    Add script to generate simulated motion for testing MC/registration

---
+++ test/data/sim_motion.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+"""
+Simulate motion to test MC/registration algorithms
+"""
+
+import sys
+
+import nibabel as nib
+import numpy as np 
+import scipy.ndimage
+
+if len(sys.argv) != 4:
+    print("Usage: sim_motion.py <input filename> <msize> <output filename>")
+    sys.exit(1)
+
+infile, msize, outfile = sys.argv[1:]
+msize = float(msize)
+
+f = nib.load(infile)
+nvols = f.shape[3]
+
+d = f.get_data()
+o = np.zeros(d.shape)
+
+for v in range(nvols):
+    vdata = d[:,:,:,v]
+    shift = np.random.normal(scale=msize, size=3)
+    odata = scipy.ndimage.interpolation.shift(vdata, shift)
+    o[:,:,:,v] = odata
+
+of = nib.Nifti1Image(o, f.header.get_best_affine())
+of.to_filename(outfile)
