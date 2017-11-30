05ca6ebeece4fbc000dbc7314520bf66228d8396
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-11-30 11:34:05 +0000

    Move preprocessing into a separate tab

--- quantiphyse/packages/plugins/qpasl/widget.py
+++ quantiphyse/packages/plugins/qpasl/widget.py
@@ -113,14 +113,19 @@ class ASLWidget(QpWidget):
         cite = Citation(FAB_CITE_TITLE, FAB_CITE_AUTHOR, FAB_CITE_JOURNAL)
         vbox.addWidget(cite)
 
-        preprocBox = QtGui.QGroupBox("Preprocessing")
+        self.tabs = QtGui.QTabWidget()
+        vbox.addWidget(self.tabs)
+
+        # Preprocessing tab
+        preprocTab = QtGui.QWidget()
         grid = QtGui.QGridLayout()
-        preprocBox.setLayout(grid)
+        preprocTab.setLayout(grid)
 
         grid.addWidget(QtGui.QLabel("Data format"), 0, 0)
         self.tc_combo = QtGui.QComboBox()
         self.tc_combo.addItem("Tag-control pairs")
         self.tc_combo.addItem("Tag-control subtracted")
+        self.tc_combo.addItem("Multiphase")
         self.tc_combo.currentIndexChanged.connect(self.update_ui)
         grid.addWidget(self.tc_combo, 0, 1)
         self.tc_ord_combo = QtGui.QComboBox()
@@ -129,26 +134,38 @@ class ASLWidget(QpWidget):
         self.tc_ord_combo.currentIndexChanged.connect(self.update_ui)
         grid.addWidget(self.tc_ord_combo, 0, 2)
 
-        grid.addWidget(QtGui.QLabel("Data grouping\n(top = outermost)"), 1, 0, alignment=QtCore.Qt.AlignTop)
+        self.ntis = NumericOption("Number of TIs/PLDs", grid, ypos=1, xpos=0, default=1, intonly=True, minval=1)
+        
+        grid.addWidget(QtGui.QLabel("Data grouping\n(top = outermost)"), 2, 0, alignment=QtCore.Qt.AlignTop)
         self.group_list = OrderList()
-        grid.addWidget(self.group_list, 1, 1)
+        grid.addWidget(self.group_list, 2, 1)
         self.list_btns = OrderListButtons(self.group_list)
-        grid.addLayout(self.list_btns, 1, 2)
+        grid.addLayout(self.list_btns, 2, 2)
         # Have to set items after adding to grid or sizing doesn't work right
         self.group_list.setItems([self.groups[g] for g in self.default_order])
         self.group_list.sig_changed.connect(self.update_ui)
 
+        grid.addWidget(QtGui.QLabel("Data order preview"), 3, 0)
         self.data_preview = AslDataPreview(self.default_order, True)
-        grid.addWidget(self.data_preview, 2, 0, 1, 3)
+        grid.addWidget(self.data_preview, 4, 0, 1, 3)
 
-        vbox.addWidget(preprocBox)
+        self.preproc_btn = QtGui.QPushButton("Preprocess data")
+        self.preproc_btn.clicked.connect(self.preprocess)
+        self.preprocessed = False
+        grid.addWidget(self.preproc_btn, 5, 0)
 
-        dataBox = QtGui.QGroupBox("Acquisition")
-        grid = QtGui.QGridLayout()
-        dataBox.setLayout(grid)
+        grid.setRowStretch(6, 1)
+
+        self.tabs.addTab(preprocTab, "Preprocessing")
+
+        # Model-based analysis tab
+
+        analysisTab = QtGui.QWidget()
+        a_vbox = QtGui.QVBoxLayout()
+        analysisTab.setLayout(a_vbox)
 
         # FIXME add multiband readout options?
-
+        grid = QtGui.QGridLayout()
         grid.addWidget(QtGui.QLabel("Labelling"), 0, 0)
         self.lbl_combo = QtGui.QComboBox()
         self.lbl_combo.addItem("cASL/pcASL")
@@ -164,13 +181,9 @@ class ASLWidget(QpWidget):
         grid.addWidget(QtGui.QLabel("Bolus Durations"), 2, 0)
         self.taus = NumberList([1.4,])
         grid.addWidget(self.taus, 2, 1, 1, 2)
+        a_vbox.addLayout(grid)
 
-        vbox.addWidget(dataBox)
-
-        analysisBox = QtGui.QGroupBox("Analysis")
         grid = QtGui.QGridLayout()
-        analysisBox.setLayout(grid)
-
         self.bat = NumericOption("Bolus arrival time (s)", grid, ypos=0, xpos=0, default=1.3, decimals=2)
         self.ie = NumericOption("Inversion efficiency", grid, ypos=1, xpos=0, default=0.85, decimals=2)
         self.t1 = NumericOption("T1 (s)", grid, ypos=0, xpos=2, default=1.3, decimals=2)
@@ -186,23 +199,27 @@ class ASLWidget(QpWidget):
         grid.addWidget(self.fixtau_cb, 4, 2, 1, 2)
         #self.pvc_cb = QtGui.QCheckBox("Partial volume correction")
         #grid.addWidget(self.pvc_cb, 5, 2, 1, 2)
+        a_vbox.addLayout(grid)
 
-        vbox.addWidget(analysisBox)
+        self.runbox = RunBox(self.get_process, self.get_rundata, title="Run ASL modelling", save_option=True)
+        a_vbox.addWidget(self.runbox)
+        a_vbox.addStretch(1)
 
-        runBox = RunBox(self.get_process, self.get_rundata, title="Run ASL modelling", save_option=True)
-        vbox.addWidget(runBox)
-        vbox.addStretch(1)
-
-        self.rundata = {}
-
-        # General defaults
-        self.rundata["save-mean"] = ""
-        self.rundata["save-model-fit"] = ""
-        self.rundata["noise"] = "white"
-        self.rundata["max-iterations"] = "20"
-        self.rundata["model"] = "aslrest"
-
+        self.tabs.addTab(analysisTab, "Analysis")
         self.update_ui()
+
+    def activate(self):
+        self.ivm.sig_main_data.connect(self.main_data_changed)
+
+    def deactivate(self):
+        self.ivm.sig_main_data.disconnect(self.main_data_changed)
+        
+    def main_data_changed(self, data):
+        if data != "asldata":
+            # Main data has changed and it is NOT out preprocessed data! That
+            # means it needs preprocessing before we can send it to Fabber
+            self.preprocessed = False
+            self.update_ui()
 
     def update_ui(self):
         """ Update visibility / enabledness of widgets """
@@ -221,6 +238,8 @@ class ASLWidget(QpWidget):
         else:
             self.plds_label.setText("TIs")
 
+        self.runbox.runBtn.setEnabled(self.preprocessed)
+
     def get_vol_idx(self, tidx, ntis, ridx, nrepeats, tcidx, ntcs):
         idx = 0
         for code in self.data_preview.order:
@@ -235,43 +254,40 @@ class ASLWidget(QpWidget):
                 idx += tcidx
         return idx
 
-    def get_process(self):
-        return self.FabberProcess(self.ivm)
-
-    def get_rundata(self):
-        self.update_options()
-        return self.rundata
-
-    def update_options(self):
-        # First preprocess the data to put it in the format the Fabber model expects.
-        # This is outer grouping of TIs, then by repeats and finally tag/control pairs
+    def preprocess(self):
+        """
+        Preprocess the data to put it in the format the Fabber model expects.
+        This is outer grouping of TIs, then by repeats and finally tag/control pairs
+        
+        FIXME multiphase not yet supported
+        """
         nvols = self.ivm.main.nvols
-        ntis = len(self.plds.values())
+        ntis = self.ntis.value()
         if nvols % ntis != 0:
             raise QpException("Number of volumes (%i) not consistent with %i PLDs" % (nvols, ntis))
-        nrepeats = int(nvols / ntis)
+        self.nrepeats = int(nvols / ntis)
 
         tcpairs = ctpairs = False
         if self.tc_combo.currentIndex() == 0:
             # Need to do tag/control subtraction
-            if nrepeats % 2 != 0:
+            if self.nrepeats % 2 != 0:
                 raise QpException("Number of volumes (%i) not consistent with %i PLDs and tag/control pairs" % (nvols, ntis))
             nvols = int(nvols/2)
-            nrepeats = int(nrepeats/2)
+            self.nrepeats = int(self.nrepeats/2)
             if self.tc_ord_combo.currentIndex() == 0:
                 tcpairs = True
             else:
                 ctpairs = True
 
-        debug("ntis=%i, nrepeats=%i, tcpairs=%i, ctpairs=%i, nvols=%i" % (ntis, nrepeats, tcpairs, ctpairs, nvols))
+        debug("ntis=%i, nrepeats=%i, tcpairs=%i, ctpairs=%i, nvols=%i" % (ntis, self.nrepeats, tcpairs, ctpairs, nvols))
         asldata = np.zeros(list(self.ivm.grid.shape) + [nvols, ])
         out_idx = 0
         if tcpairs or ctpairs: npairs = 2
         else: npairs = 1
         for tidx in range(ntis):
-            for ridx in range(nrepeats):
-                idx1 = self.get_vol_idx(tidx, ntis, ridx, nrepeats, 0, npairs)
-                idx2 = self.get_vol_idx(tidx, ntis, ridx, nrepeats, 1, npairs)
+            for ridx in range(self.nrepeats):
+                idx1 = self.get_vol_idx(tidx, ntis, ridx, self.nrepeats, 0, npairs)
+                idx2 = self.get_vol_idx(tidx, ntis, ridx, self.nrepeats, 1, npairs)
                 debug("tidx=%i, ridx=%i, tc1=%i, tc2=%i" % (tidx, ridx, idx1, idx2))
                 if ctpairs:
                     # Do control - tag
@@ -288,14 +304,31 @@ class ASLWidget(QpWidget):
 #        meandiff = np.mean(asldata, 3)
 #        self.ivm.add_data(meandiff, name="diff", make_current=True)
         self.ivm.add_data(asldata, name="asldata", make_main=True)
-        
+        self.preprocessed = True
+        self.update_ui()
+       
+    def get_process(self):
+        return self.FabberProcess(self.ivm)
+
+    def get_rundata(self):
+        # General defaults
+        self.rundata = {}
+        self.rundata["save-mean"] = ""
+        self.rundata["save-model-fit"] = ""
+        self.rundata["noise"] = "white"
+        self.rundata["max-iterations"] = "20"
+        self.rundata["model"] = "aslrest"
+
         # Acquisition parameters
         if self.lbl_combo.currentIndex() == 0:
             self.rundata["casl"] = ""
         else:
-            del self.rundata["casl"]
+            self.rundata.pop("casl", None)
 
         tis = self.plds.values()
+        if len(tis) != self.ntis.value():
+            raise QpException("Number of TIs/PLDs must match number specified in preprocessing")
+           
         taus = self.taus.values()
         singletau = None
         if len(taus) == 1:
@@ -313,7 +346,7 @@ class ASLWidget(QpWidget):
                 else: ti += taus[idx]
             self.rundata["ti%i" % (idx+1)] = str(ti)
 
-        self.rundata["repeats"] = str(nrepeats)
+        self.rundata["repeats"] = str(self.nrepeats)
 
         # Starting values
         self.rundata["t1"] = str(self.t1.spin.value())
@@ -335,22 +368,22 @@ class ASLWidget(QpWidget):
             self.rundata["infert1"] = ""
             self.rundata["inct1"] = ""
         else:
-            del self.rundata["infert1"]
-            del self.rundata["inct1"]
+            self.rundata.pop("infert1", None)
+            self.rundata.pop("inct1", None)
 
         if self.mv_cb.isChecked():
             self.rundata["inferart"] = ""
             self.rundata["incart"] = ""
         else:
-            del self.rundata["inferart"]
-            del self.rundata["incart"]
+            self.rundata.pop("inferart", None)
+            self.rundata.pop("incart", None)
             
         if not self.fixtau_cb.isChecked():
             self.rundata["infertau"] = ""
             self.rundata["inctau"] = ""
         else:
-            del self.rundata["infetau"]
-            del self.rundata["inctau"]
+            self.rundata.pop("infetau", None)
+            self.rundata.pop("inctau", None)
 
         for item in self.rundata.items():
             debug("%s: %s" % item)
