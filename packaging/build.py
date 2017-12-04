696b3f9242d4ca5648fb22d96a9413bb984b6e14
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-12-04 09:06:01 +0000

    Move packaging out of spec file and support snapshots/maxi-package

--- packaging/build.py
+++ packaging/build.py
@@ -1,12 +1,79 @@
+"""
+Master build script for Quantiphyse packages
+
+Can specify:
+  --snapshot to name packages as 'snapshot' rather than embedding the version number
+  --maxi to bundle selected plugins (Basically the Fabber frontends)
+"""
 import os, sys
 
 pkgdir = os.path.abspath(os.path.dirname(__file__))
-qpdir = os.path.abspath(os.path.join(pkgdir, os.pardir))
-sys.path.append(qpdir)
+rootdir = os.path.abspath(os.path.join(pkgdir, os.pardir))
+distdir = os.path.join(rootdir, "dist")
+package_name = "quantiphyse"
 
-os.system("rm -rf %s/dist" % qpdir)
-os.system("python %s/update_version.py" % pkgdir)
-print("Building extensions")
-os.system("python %s/setup.py build_ext --inplace --force 2>%s/build.err 1>%s/build.log" % (qpdir, pkgdir, pkgdir))
-print("Freezing code and creating packages")
-os.system("pyinstaller -y %s/quantiphyse.spec 2>%s/freeze.err 1>%s/freeze.log" % (pkgdir, pkgdir, pkgdir))
+sys.path.append(rootdir)
+
+archive_method="zip"
+build_platform_package=None
+if sys.platform.startswith("win"):
+    platform="win32"
+    import create_msi
+    build_platform_package = create_msi.create_msi
+elif sys.platform.startswith("linux"):
+    sysname=platform.linux_distribution()[0].split()[0].lower()
+    platform="linux"
+    archive_method="gztar"
+    import create_deb
+    build_platform_package = create_deb.create_deb
+elif sys.platform.startswith("darwin"):
+    platform="osx"
+    import create_dmg
+    build_platform_package = create_dmg.create_dmg
+
+os.system("rm -rf %s/dist" % rootdir)
+
+from update_version import update_version
+full_version, std_version = update_version(package_name, rootdir)
+
+sys.stdout.write("Building extensions...")
+sys.stdout.flush()
+os.system("python %s/setup.py build_ext --inplace --force 2>%s/build.err 1>%s/build.log" % (rootdir, pkgdir, pkgdir))
+print("DONE")
+
+sys.stdout.write("Freezing code...")
+sys.stdout.flush()
+os.system("pyinstaller -y %s/%s.spec 2>%s/freeze.err 1>%s/freeze.log" % (pkgdir, package_name, pkgdir, pkgdir))
+print("DONE")
+
+if "--maxi" in sys.argv:
+    print("Creating a maxi-package")
+    # This is necessarily a bit of a hack but so is the concept of a maxi-package
+    plugins = ["fabber-ap", "basil-qp", "quanticest"]
+    for plugin in plugins:
+        sys.stdout.write("  - Building and bundling %s..." % plugin)
+        sys.stdout.flush()
+        plugindir = os.path.join(rootdir, os.pardir, plugin)
+        os.system("python %s/packaging/build.py" % plugindir)
+        shutil.copytree("%s/dist/%s" (plugin, plugin), 
+                        "%s/%s/packages/plugins/%s" % (rootdir, package_name, plugin))
+        if platform == "osx":
+            # Need to put it into the OSX bundle as well
+            shutil.copytree("%s/dist/%s" (plugin, plugin), 
+                            "%s/%s.app/Contents/Resources/packages/plugins/%s" % (rootdir, package_name, plugin))            
+        print("DONE")
+
+sys.stdout.write("Creating compressed archive...")
+sys.stdout.flush()
+shutil.make_archive("%s/%s-%s-%s" % (distdir, package_name, version_str, sysname), 
+                    archive_method, "%s/%s" % (distdir, package_name))
+print("DONE")
+
+if build_platform_package is not None:
+    sys.stdout.write("Creating platform-specific package...")
+    sys.stdout.flush()
+    version_pkg_fname = std_version
+    if "--snapshot" in sys.argv:
+        version_pkg_fname = "snapshot"
+    build_platform_package(distdir, pkgdir, std_version, sysname, version_pkg_fname)
+    print("DONE")
--- packaging/quantiphyse.spec
+++ packaging/quantiphyse.spec
@@ -2,8 +2,6 @@
 import sys
 import os
 import struct
-import subprocess
-import re
 import shutil
 import platform
 
@@ -11,29 +9,8 @@ import platform
 OSX_LIBPNG_HACK = True
 
 pkgdir = os.path.abspath(os.path.dirname(SPEC))
-sys.path.append(pkgdir)
 qpdir = os.path.abspath(os.path.join(pkgdir, os.pardir))
 distdir = os.path.join(qpdir, "dist")
-
-# This is copied from update_version for now until we sort out how to import it...
-def get_std_version():
-    """ 
-    Get standardized version string in form maj.min.patch-release
-    """
-    v = subprocess.check_output('git describe --dirty', shell=True).strip(" \n")
-    p = re.compile("v?(\d+\.\d+\.\d+(-\d+)?).*")
-    m = p.match(v)
-    if m is not None:
-        return  m.group(1)
-    else:
-        raise RuntimeError("Failed to parse version string %s" % v)
-
-# Update version info from git tags and get standardized version for packages
-version_str = get_std_version()
-if len(version_str.split("-", 1)) > 0:
-    # This is a snapshot, name accordingly (real version is still embeddd in the python)
-    version_str_orig = version_str
-    version_str = "snapshot"
 
 # Generic configuration
 bin_files = [
@@ -74,8 +51,6 @@ sys.path.append("%s/lib/python/" % os.environ["FSLDIR"])
 hidden_imports.append('fabber')
 
 # Platform-specific configuration
-platform_package = None
-archive_method="zip"
 osx_bundle = False
 block_cipher = None
 
@@ -83,9 +58,6 @@ block_cipher = None
 bits = struct.calcsize("P") * 8
 
 if sys.platform.startswith("win"):
-    sysname="win32"
-    import create_msi
-    platform_package = create_msi.create_msi
     home_dir = os.environ.get("USERPROFILE", "")
     anaconda_dir='%s/AppData/Local/Continuum/Anaconda2/' % home_dir
     bin_files.append(('%s/Library/bin/mkl_avx2.dll' % anaconda_dir, '.' ))
@@ -99,18 +71,11 @@ if sys.platform.startswith("win"):
                            'packaging.specifiers', 'packaging.utils',
                            'packaging.requirements', 'packaging.markers'],
 elif sys.platform.startswith("linux"):
-    sysname=platform.linux_distribution()[0].split()[0].lower()
-    import create_deb
-    platform_package = create_deb.create_deb
-    archive_method="gztar"
     hidden_imports.append('FileDialog')
     hidden_imports.append('pywt._extensions._cwt')
     bin_files.append(("%s/lib/libfabber*.so" % fsldir, "fabber/lib"))
     bin_files.append(("%s/bin/fabber" % fsldir, "fabber/bin"))
 elif sys.platform.startswith("darwin"):
-    sysname="osx"
-    import create_dmg
-    platform_package = create_dmg.create_dmg
     osx_bundle = True
     home_dir = os.environ.get("HOME", "")
     anaconda_dir='%s/anaconda2/' % home_dir
@@ -173,15 +138,3 @@ if sys.platform.startswith("darwin") and OSX_LIBPNG_HACK:
     if osx_bundle:
         dest = os.path.join(distdir, "quantiphyse.app/Contents/MacOS/")
         shutil.copy(libpng, dest)
-
-sys.stdout.write("Creating compressed archive...")
-sys.stdout.flush()
-shutil.make_archive("%s/quantiphyse-%s-%s" % (distdir, version_str, sysname), 
-                    archive_method, "%s/quantiphyse" % distdir)
-print("DONE")
-
-if platform_package is not None:
-    sys.stdout.write("Creating platform-specific package...")
-    sys.stdout.flush()
-    platform_package(distdir, pkgdir, version_str_orig, sysname, version_str)
-    print("DONE")
