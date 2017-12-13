52e02e20f2b862bf7b29dfa3aa62d40e1d30f316
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-12-13 13:12:01 +0000

    Renamed examples

--- test/system_tests/run_examples.py
+++ test/system_tests/run_examples.py
@@ -11,7 +11,7 @@ script_dir = os.path.abspath(os.path.dirname(__file__))
 qpdir = os.path.abspath(os.path.join(script_dir, os.pardir, os.pardir))
 os.chdir(qpdir)
 
-examples = glob.glob(os.path.join(qpdir, "examples", "eg*.yaml"))
+examples = glob.glob(os.path.join(qpdir, "examples", "batch_scripts", "*.yaml"))
 for ex in sorted(examples):
     print("**** Running example: %s" % ex)
     os.system("python -u qp.py --batch=%s" % ex)
