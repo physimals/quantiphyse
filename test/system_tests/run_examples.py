e8357160f1d0b5bb70a73217926505589cc11d89
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-12-20 09:24:16 +0000

    Remove output dir before running examples

--- test/system_tests/run_examples.py
+++ test/system_tests/run_examples.py
@@ -5,11 +5,14 @@ Run all the examples
 import sys
 import os
 import glob
+import shutil
 
 script_dir = os.path.abspath(os.path.dirname(__file__))
 
 qpdir = os.path.abspath(os.path.join(script_dir, os.pardir, os.pardir))
 os.chdir(qpdir)
+
+shutil.rmtree(os.path.join(qpdir, "test", "examples_output"))
 
 examples = glob.glob(os.path.join(qpdir, "examples", "batch_scripts", "*.yaml"))
 for ex in sorted(examples):
