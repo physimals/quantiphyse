0f0a8af9ba5af6f3046736705632f1859b374d84
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-11-21 14:40:13 +0000

    Add 'current volume only' option for statistics

--- quantiphyse/packages/core/analysis/widgets.py
+++ quantiphyse/packages/core/analysis/widgets.py
@@ -205,9 +205,14 @@ class DataStatistics(QpWidget):
         hbox.addWidget(self.roi)
         hbox.setStretch(1, 1)
         hbox.setStretch(3, 1)
-
-        #hbox.addStretch(1)
         main_vbox.addLayout(hbox)
+
+        hbox = QtGui.QHBoxLayout()
+        self.current_vol = QtGui.QCheckBox("Current volume only")
+        hbox.addWidget(self.current_vol)
+        hbox.addStretch(1)
+        main_vbox.addLayout(hbox)
+        self.current_vol.stateChanged.connect(self.update_all)
 
         # Summary stats
         stats_box = QtGui.QGroupBox()
@@ -296,6 +301,8 @@ class DataStatistics(QpWidget):
     def focus_changed(self, _):
         if self.stats_table_ss.isVisible():
             self.update_stats_current_slice()
+        if self.stats_table.isVisible() and self.current_vol.isChecked():
+            self.update_stats()
 
     def update_all(self):
         if self.stats_table.isVisible():
@@ -350,6 +357,8 @@ class DataStatistics(QpWidget):
     def populate_stats_table(self, process, options):
         options["data"] = self.data.value
         options["roi"] = self.roi.value
+        if self.current_vol.isChecked():
+            options["vol"] = self.ivl.focus()[3]
         process.run(options)
 
 class RoiAnalysisWidget(QpWidget):
