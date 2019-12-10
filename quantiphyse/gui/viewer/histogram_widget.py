d23a3e59845bd0013d70c3bc3760748d552d0902
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-12-10 10:44:57 +0000

    Refactor histogram so it controls view metadata rather than the image items themselves

--- quantiphyse/gui/viewer/histogram_widget.py
+++ quantiphyse/gui/viewer/histogram_widget.py
@@ -1,5 +1,5 @@
 """
-Quantiphyse - Extension to the PyQtGraph histogram to support multiple images
+Quantiphyse - Custom histogram display widget
 
 Copyright (c) 2013-2018 University of Oxford
 """
@@ -13,38 +13,96 @@ import pyqtgraph as pg
 
 from quantiphyse.data.qpdata import remove_nans
 
-class MultiImageHistogramWidget(pg.HistogramLUTWidget):
+class HistogramWidget(pg.HistogramLUTWidget):
     """
-    A histogram widget which has one array of 'source' data
-    (which it gets the histogram itself and the initial levels from)
-    and multiple image item views which are affected by changes to the
-    levels or LUT
+    Displays histogram and colour map from a view metadata object
+    and allows them to be updated interactivately
+
+    The histogram is taken from a QpData instance. Normally the 
+    colourmap is also taken from the .view property of the QpData, however
+    it is possible to set the custom_view property to substitute another
+    set of viewing metadata. This is used to implement the 'background'
+    main data view.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, ivl, *args, **kwargs):
         kwargs["fillHistogram"] = False
-        super(MultiImageHistogramWidget, self).__init__(*args, **kwargs)
-
+        super(HistogramWidget, self).__init__(*args, **kwargs)
         self.setBackground(None)
-        self._vol = 0
+
         self._qpdata = None
-        self.updating = False
+        self._custom_view = None
+        self._vol = 0
+        self._updating = False
 
-        #self.sigLevelChangeFinished.connect(self._levels_changed)
-        #self.sigLevelsChanged.connect(self._levels_changed)
-        #self.sigLookupTableChanged.connect(self._lut_changed)
-        #self._update_histogram()
+        ivl.sig_focus_changed.connect(self._focus_changed)
+        self.sigLevelChangeFinished.connect(self._levels_changed)
+        self.sigLevelsChanged.connect(self._levels_changed)
+        self.sigLookupTableChanged.connect(self._lut_changed)
 
-    def set_data(self, qpdata):
+    @property
+    def qpdata(self):
         """
-        Add a view to the histogram
+        QpData object containing the data to display.
+
+        If the view metadata is not set explicitly, the view property
+        of the qpdata is used
         """
+        return self._qpdata
+
+    @qpdata.setter
+    def qpdata(self, qpdata):
+        if self.view is not None:
+            self.view.sig_changed.disconnect(self._view_changed)
+
         self._qpdata = qpdata
+        if self.view is not None:
+            self.view.sig_changed.connect(self._view_changed)
         self._update_cmap()
         self._update_histogram()
 
-    def set_vol(self, vol):
-        self._vol = vol
-        self._update_histogram()
+    @property
+    def view(self):
+        """
+        Metadata object containing view properties
+        """
+        if self._qpdata is not None and self._custom_view is None:
+            return self._qpdata.view
+        else:
+            return self._custom_view
+
+    @property
+    def custom_view(self):
+        return self._custom_view
+
+    @custom_view.setter
+    def custom_view(self, view):
+        if self.view is not None:
+            self.view.sig_changed.disconnect(self._view_changed)
+
+        self._custom_view = view
+        if self.view is not None:
+            self.view.sig_changed.connect(self._view_changed)
+        self._update_cmap()
+
+    @property
+    def vol(self):
+        """
+        Current volume index being displayed on the histogram
+        """
+        return self._vol
+
+    @vol.setter
+    def vol(self, vol):
+        if vol != self._vol:
+            self._vol = vol
+            self._update_histogram()
+
+    def _focus_changed(self, focus):
+        self.vol = focus[3]
+
+    def _view_changed(self, key, _value):
+        if key in ("cmap", "cmap_range"):
+            self._update_cmap()
 
     def _update_histogram(self):
         if self._qpdata is not None:
@@ -57,35 +115,38 @@ class MultiImageHistogramWidget(pg.HistogramLUTWidget):
             self.plot.setData(*hist)
 
     def _update_cmap(self):
-        try:
-            self.updating = True
+        if self.view is not None:
+            try:
+                self._updating = True
 
-            cmap_range = self._md("cmap_range")
-            if cmap_range is not None:
-                self.region.setRegion(cmap_range)
+                cmap_range = self.view.cmap_range
+                if cmap_range is not None:
+                    self.region.setRegion(cmap_range)
 
-            cmap = self._md("cmap")
-            if cmap != "custom":
-                try:
-                    self.gradient.loadPreset(cmap)
-                except KeyError:
-                    self._set_matplotlib_gradient(cmap)
+                cmap = self.view.cmap
+                if cmap != "custom":
+                    try:
+                        self.gradient.loadPreset(cmap)
+                    except KeyError:
+                        self._set_matplotlib_gradient(cmap)
+                else:
+                    # FIXME some other components sets a custom cmap
+                    # need to convert to ticks and update gradient
+                    pass
 
-            #self.lut = None
-            # FIXME how to pass the LUT back to the image
-            #for img in self.imgs:
-            #    img.setLookupTable(self._get_image_lut, update=True)
-        finally:
-            self.updating = False
+            finally:
+                self._updating = False
 
     def _levels_changed(self):
-        if not self.updating:
+        if self.view is not None and not self._updating:
             self.view.cmap_range = list(self.region.getRegion())
 
     def _lut_changed(self):
-        if not self.updating:
-            self._set_md("cmap", "custom")
-            self._set_md("lut", self.gradient.ticks)
+        if self.view is not None and not self._updating:
+            # FIXME
+            pass
+            #self.view.lut = [tuple(row) for row in self.gradient.getLookupTable(255)]
+            #self.view.cmap = "custom"
 
     def _set_matplotlib_gradient(self, name):
         """
@@ -98,3 +159,14 @@ class MultiImageHistogramWidget(pg.HistogramLUTWidget):
         cmap = getattr(cm, name)
         ticks = [(pos, [255 * v for v in cmap(pos)]) for pos in np.linspace(0, 1, 10)]
         self.gradient.restoreState({'ticks': ticks, 'mode': 'rgb'})
+
+class CurrentDataHistogramWidget(HistogramWidget):
+    """
+    Displays histogram and colour map from current data
+    """
+    def __init__(self, ivl, *args, **kwargs):
+        super(CurrentDataHistogramWidget, self).__init__(ivl, *args, **kwargs)
+        ivl.ivm.sig_current_data.connect(self._current_data_changed)
+
+    def _current_data_changed(self, qpdata):
+        self.qpdata = qpdata
